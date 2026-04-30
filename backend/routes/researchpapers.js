baff3248670d0dd51981c0dac25379c0b5e6ebc3
dwivediprashant <prashantdwivedi.0219@gmail.com>
2026-05-01 03:54:23 +0530

    refactor(api) : used params in api call + added page field to support pagination

--- backend/routes/researchpapers.js
+++ backend/routes/researchpapers.js
@@ -8,17 +8,26 @@ const OPENALEX_API_KEY = process.env.OPENALEX_API_KEY;
 
 router.get("/", async (req, res) => {
   try {
-    const { field } = req.query;
+    const { field, pageNum } = req.query;
     let data = null;
 
     if (field) {
-      const response = await axios.get(
-        `${OPENALEX_BASE_URL}/works?filter=type:article,open_access.is_oa:true&search=${field}&select=display_name,authorships,publication_year,primary_location,open_access,cited_by_count,primary_topic&api_key=${OPENALEX_API_KEY}`,
-      );
+      const response = await axios.get(`${OPENALEX_BASE_URL}/works`, {
+        params: {
+          filter: "type:article,open_access.is_oa:true",
+          search: field,
+          select:
+            "display_name,authorships,publication_year,primary_location,open_access,cited_by_count,primary_topic",
+          per_page: 10,
+          api_key: OPENALEX_API_KEY,
+          page: pageNum,
+        },
+      });
+
       data = response.data;
     }
 
-    res.status(200).json({ success: "true", data: data.results });
+    res.status(200).json({ success: "true", data });
   } catch (error) {
     // console.log(error);
     res
